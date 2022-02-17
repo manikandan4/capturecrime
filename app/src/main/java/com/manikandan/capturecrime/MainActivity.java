371673f246a7e91e26dfc6a0e98aba2613acf677
manikandan Sadhasivam <mailtokandan84@gmail.com>
2022-02-17 14:11:25 +0800

    chapter08 -work in progress

--- app/src/main/java/com/manikandan/capturecrime/MainActivity.java
+++ app/src/main/java/com/manikandan/capturecrime/MainActivity.java
@@ -1,14 +1,13 @@
 package com.manikandan.capturecrime;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
+import com.manikandan.capturecrime.fragments.CrimeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
